1dcc526209c329f679fb177226d701cb56fbf9c5
Ryo Sato <ryo.sato@datachain.jp>
2022-06-27 11:13:25 +0900

    update tm2harmony config & add test-tx
    Signed-off-by: Ryo Sato <ryo.sato@datachain.jp>

--- tests/cases/tm2harmony/configs/tpl/ibc-1.json.tpl
+++ tests/cases/tm2harmony/configs/tpl/ibc-1.json.tpl
@@ -10,6 +10,9 @@
     "beacon_private_key": "1f84c95ac16e6a50f08d44c7bde7aff8742212fda6e4321fde48bf83bef266dc",
     "ibc_host_address": "",
     "ibc_handler_address": "",
+    "token_address": "",
+    "ics20_bank_address": "",
+    "ics20_transfer_bank_address": "",
     "gas_limit": 5000000,
     "gas_price": 1000000000
   },
--- tests/chains/harmony/scripts/saveContractAddresses.sh
+++ tests/chains/harmony/scripts/saveContractAddresses.sh
@@ -12,3 +12,6 @@ CONTRACT_DIR=$2
 OUTPUT_DIR=$3
 jq -r ".networks | .[\"${NETWORK_ID}\"].address" < ${CONTRACT_DIR}/build/contracts/IBCHost.json > ${OUTPUT_DIR}/IBCHost
 jq -r ".networks | .[\"${NETWORK_ID}\"].address" < ${CONTRACT_DIR}/build/contracts/IBCHandler.json > ${OUTPUT_DIR}/IBCHandler
+jq -r ".networks | .[\"${NETWORK_ID}\"].address" < ${CONTRACT_DIR}/build/contracts/SimpleToken.json > ${OUTPUT_DIR}/SimpleToken
+jq -r ".networks | .[\"${NETWORK_ID}\"].address" < ${CONTRACT_DIR}/build/contracts/ICS20Bank.json > ${OUTPUT_DIR}/ICS20Bank
+jq -r ".networks | .[\"${NETWORK_ID}\"].address" < ${CONTRACT_DIR}/build/contracts/ICS20TransferBank.json > ${OUTPUT_DIR}/ICS20TransferBank
